88ec66ab542602ef742521d0a964db91ae7074c6
harperphamm <124912894+harperphamm@users.noreply.github.com>
2023-05-07 15:42:48 -0400

    Create ToDoItem.js

---
+++ ca02/models/ToDoItem.js
@@ -0,0 +1,19 @@
+
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var toDoItemSchema = Schema({
+  item: String,
+  completed: Boolean,
+  createdAt: Date,
+  priority: Number,
+  description: String,
+  category: String,
+  amount: Number,
+  date: Date,
+  userId: ObjectId
+});
+
+module.exports = mongoose.model('ToDoItem', toDoItemSchema);
