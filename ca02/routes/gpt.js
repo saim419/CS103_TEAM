5ad08715a377dbfd4c652913e35ab7c2cd982e21
harperphamm <124912894+harperphamm@users.noreply.github.com>
2023-05-07 16:25:39 -0400

    Update gpt.js

--- ca02/routes/gpt.js
+++ ca02/routes/gpt.js
@@ -1,11 +1,11 @@
 const openai = require('openai');
 console.log(input)
-const generateShoeList = async (input) => {
+const generateClothingList = async (input) => {
     // Set up the OpenAI API client
     openai.apiKey = process.env.OPENAI_API_KEY;
 
     // Define the prompt for generating a list of shoes
-    const prompt = `List the top 10 shoes similar to ${input}`;
+    const prompt = `List the top 10 clothing items according to ${input}`;
 
     // Use the OpenAI API to generate a response
     const response = await openai.complete({
@@ -18,10 +18,10 @@ const generateShoeList = async (input) => {
     });
 
     // Extract the list of shoes from the response
-    const shoeList = response.choices[0].text.trim();
+    const clothingList = response.choices[0].text.trim();
 
     // Return the generated shoe list
-    return shoeList;
+    return clothingList;
 };
 
-module.exports = { generateShoeList };
+module.exports = { generateClothingList };
